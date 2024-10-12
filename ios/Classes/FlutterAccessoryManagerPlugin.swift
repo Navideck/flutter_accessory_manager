17d7e3030462f1c6fdff36608d0d4d2e9bb74982
Foti Dim <fdimanidis@gmail.com>
2024-10-12 21:06:18 +0200

    Rename manager

--- ios/Classes/FlutterAccessoryManagerPlugin.swift
+++ ios/Classes/FlutterAccessoryManagerPlugin.swift
@@ -4,7 +4,7 @@ import UIKit
 
 public class FlutterAccessoryManagerPlugin: NSObject, FlutterPlugin, FlutterAccessoryPlatformChannel {
   var callbackChannel: FlutterAccessoryCallbackChannel
-  var _manager = EAAccessoryManager.shared()
+  private var manager = EAAccessoryManager.shared()
 
   init(callbackChannel: FlutterAccessoryCallbackChannel) {
     self.callbackChannel = callbackChannel
@@ -21,8 +21,8 @@ public class FlutterAccessoryManagerPlugin: NSObject, FlutterPlugin, FlutterAcce
   func showBluetoothAccessoryPicker(completion: @escaping (Result<Void, any Error>) -> Void) {
     NotificationCenter.default.addObserver(self, selector: #selector(accessoryConnected), name: NSNotification.Name.EAAccessoryDidConnect, object: nil)
     NotificationCenter.default.addObserver(self, selector: #selector(accessoryDisconnected), name: NSNotification.Name.EAAccessoryDidDisconnect, object: nil)
-    _manager.registerForLocalNotifications()
-    _manager.showBluetoothAccessoryPicker(withNameFilter: nil, completion: { error in
+    manager.registerForLocalNotifications()
+    manager.showBluetoothAccessoryPicker(withNameFilter: nil, completion: { error in
       if error != nil {
         completion(.failure(PigeonError(code: "failed", message: error?.localizedDescription ?? "Unknown Error", details: nil)))
       } else {
