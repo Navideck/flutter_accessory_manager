390690eac4788dc544b58244e8bdf13f2c2a307c
Foti Dim <fdimanidis@gmail.com>
2024-10-14 14:02:45 +0200

    wip

--- ios/Classes/FlutterAccessoryManagerPlugin.swift
+++ ios/Classes/FlutterAccessoryManagerPlugin.swift
@@ -12,25 +12,43 @@ public class FlutterAccessoryManagerPlugin: NSObject, FlutterPlugin, FlutterAcce
   }
 
   public static func register(with registrar: FlutterPluginRegistrar) {
-    var messenger: FlutterBinaryMessenger = registrar.messenger()
+    let messenger: FlutterBinaryMessenger = registrar.messenger()
     let callbackChannel = FlutterAccessoryCallbackChannel(binaryMessenger: messenger)
     let instance = FlutterAccessoryManagerPlugin(callbackChannel: callbackChannel)
     FlutterAccessoryPlatformChannelSetup.setUp(binaryMessenger: messenger, api: instance)
   }
 
   func showBluetoothAccessoryPicker(completion: @escaping (Result<Void, any Error>) -> Void) {
-    NotificationCenter.default.addObserver(self, selector: #selector(accessoryConnected), name: NSNotification.Name.EAAccessoryDidConnect, object: nil)
-    NotificationCenter.default.addObserver(self, selector: #selector(accessoryDisconnected), name: NSNotification.Name.EAAccessoryDidDisconnect, object: nil)
+    // Get this from flutter, if user picked a device, and this protocol string appeared in connectedDevices, then disconnected
+    let protocolStringToDisconnect = "com.nikon.psg-0100"   // TODO: Unhardcode it
+      NotificationCenter.default.addObserver(self, selector: #selector(accessoryConnected(_:)), name: NSNotification.Name.EAAccessoryDidConnect, object: nil)
+      NotificationCenter.default.addObserver(self, selector: #selector(accessoryDisconnected(_:)), name: NSNotification.Name.EAAccessoryDidDisconnect, object: nil)
+      
     manager.registerForLocalNotifications()
-    manager.showBluetoothAccessoryPicker(withNameFilter: nil, completion: { error in
-      if error != nil {
-        completion(.failure(PigeonError(code: "failed", message: error?.localizedDescription ?? "Unknown Error", details: nil)))
+    manager.showBluetoothAccessoryPicker(withNameFilter: nil) { error in
+      if let error = error {
+        completion(.failure(PigeonError(code: "failed", message: error.localizedDescription, details: nil)))
+        NotificationCenter.default.removeObserver(self, name: NSNotification.Name.EAAccessoryDidDisconnect, object: completion)
+        NotificationCenter.default.removeObserver(self, name: NSNotification.Name.EAAccessoryDidConnect, object: completion)
       } else {
-        completion(.success({}()))
+        self.openAndCloseEaSession(protocolStringToDisconnect: protocolStringToDisconnect)
       }
-    })
+    }
   }
-
+    
+  private func openAndCloseEaSession(protocolStringToDisconnect: String) {
+    let accessory = manager.connectedAccessories.first { acc in
+      acc.protocolStrings.contains(protocolStringToDisconnect)
+    }
+    guard let accessory else { return }
+    var session: EASession? = EASession(accessory: accessory, forProtocol: protocolStringToDisconnect)
+    print("EASession opened \(String(describing: session?.description))")
+    DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+      print("Closing EASession")
+      session = nil
+    }
+  }
+    
   func startScan() throws {
     throw PigeonError(code: "NotSupported", message: nil, details: nil)
   }
@@ -46,23 +64,27 @@ public class FlutterAccessoryManagerPlugin: NSObject, FlutterPlugin, FlutterAcce
   func getPairedDevices() throws -> [BluetoothDevice] {
     throw PigeonError(code: "NotSupported", message: nil, details: nil)
   }
-
-  func pair(address _: String, completion: @escaping (Result<Bool, any Error>) -> Void) {
+    
+  func pair(address: String, completion: @escaping (Result<Bool, any Error>) -> Void) {
     completion(.failure(PigeonError(code: "NotSupported", message: nil, details: nil)))
   }
 
-  @objc private func accessoryConnected(notification: NSNotification) {
+  @objc private func accessoryConnected(_ notification: NSNotification) {
     let connectedAccessory = notification.userInfo![EAAccessoryKey] as? EAAccessory
     print("Accessory Connected")
     guard let connectedAccessory else { return }
     callbackChannel.accessoryConnected(accessory: connectedAccessory.toEAAccessoryObject()) { _ in }
   }
 
-  @objc private func accessoryDisconnected(notification: NSNotification) {
+  @objc private func accessoryDisconnected(_ notification: NSNotification) {
     let connectedAccessory = notification.userInfo![EAAccessoryKey] as? EAAccessory
     guard let connectedAccessory else { return }
     print("Accessory Disconnected")
     callbackChannel.accessoryDisconnected(accessory: connectedAccessory.toEAAccessoryObject()) { _ in }
+      completion(.success(()))  // TODO: Get it from an instance dictionary property
+      
+    NotificationCenter.default.removeObserver(self, name: NSNotification.Name.EAAccessoryDidDisconnect, object: notification.object)
+    NotificationCenter.default.removeObserver(self, name: NSNotification.Name.EAAccessoryDidConnect, object: notification.object)
   }
 }
 
