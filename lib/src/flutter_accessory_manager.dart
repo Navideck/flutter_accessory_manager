756de10a42aa434504eae6e165372b65aa7ea498
Foti Dim <fdimanidis@gmail.com>
2024-11-26 11:49:55 +0100

    Add comments

--- lib/src/flutter_accessory_manager.dart
+++ lib/src/flutter_accessory_manager.dart
@@ -14,6 +14,8 @@ class FlutterAccessoryManager {
     return _platform.showBluetoothAccessoryPicker(withNames: withNames);
   }
 
+  /// Closes the EASession.
+  /// If no protocol string is passed then it will use the first one available
   static Future<void> closeEaSession(String? protocolString) =>
       _platform.closeEaSession(protocolString);
 
